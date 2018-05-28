ad3121eb1135e6ad95c90a03ca532055eaace51c
ihbaraba <ihbaraba@gmail.com>
2018-05-28 17:45:51 +0300

    1st day. header + main styles

--- smartgrid.js
+++ smartgrid.js
@@ -4,25 +4,24 @@ const smartgrid = require('smart-grid');
 const settings = {
     outputStyle: 'sass', /* less || scss || sass || styl */
     columns: 12, /* number of grid columns */
-    offset: '30px', /* gutter width px || % */
+    offset: '35px', /* gutter width px || % */
     mobileFirst: false, /* mobileFirst ? 'min-width' : 'max-width' */
     container: {
-        maxWidth: '1200px', /* max-width оn very large screen */
-        fields: '30px' /* side fields */
+        maxWidth: '1170px', /* max-width оn very large screen */
+        fields: '15px' /* side fields */
     },
     breakPoints: {
         lg: {
-            width: '1100px' /* -> @media (max-width: 1100px) */
+            width: '1600px' /* -> @media (max-width: 1100px) */
         },
         md: {
-            width: '960px'
+            width: '1200px'
         },
         sm: {
-            width: '780px',
-            fields: '15px' /* set fields only if you want to change container.fields */
+            width: '992px'
         },
         xs: {
-            width: '560px'
+            width: '768px'
         }
         /*
         We can create any quantity of break points.
